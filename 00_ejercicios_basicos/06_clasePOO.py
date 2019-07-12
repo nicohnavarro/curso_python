81ac43ead7b977a3b511a066665d7ff8b4b45b34
nicohnavarro <nicohnavarro@gmail.com>
2019-07-12 15:19:47 -0300

    POO

--- 00_ejercicios_basicos/06_clasePOO.py
+++ 00_ejercicios_basicos/06_clasePOO.py
@@ -1,1 +1,57 @@
 #Objetos Clase 25
+#Definimos la clase
+class Coche():
+
+    #Estado inicial atravez del constructor...
+    def __init__(self):
+        #Propiedades
+        self.__largoChasis=250  
+        self.__anchoChasis=120
+        self.__ruedas=4   #Con __ lo haces privado (Encapsulacion)
+        self.__enMarcha=False
+        self.color="Negro"
+
+    #Comportamiento o Metodos
+    #Diferencia entre metodo y funcion ->La funcion no depende de una clase
+    def Arrancar(self,arrancamos): #el self hace referencia al propio objeto(la instancia) perteneciente a la clase
+        self.__enMarcha=arrancamos
+        if(self.__enMarcha):
+            print("El coche esta en marcha")
+            chequeo=self.__chequeo_interno()
+            if(chequeo):
+                print("El chequeo ha sido positivo")
+        else:
+            return "El coche esta parado"
+
+    def Estado(self):
+        print("El coche tiene ",self.__ruedas,"ruedas")
+        print("Un ancho de ",self.__anchoChasis)
+        print("Un largo de ",self.__largoChasis)
+        print("Color :",self.color)
+        print("Estado :",self.__enMarcha)
+    
+    def __chequeo_interno(self):
+        print("Realizando chequeo interno")
+        self.gasolina="Ok"
+        self.aceite="Ok"
+        self.puertas="Cerradas"
+        if(self.gasolina=="Ok" and self.aceite=="Ok" and self.puertas=="Cerradas"):
+            return True
+        else:
+            return False
+
+
+#Mi programa
+print("--------------------------Objetos--------------------------")
+miCoche1=Coche()
+miCoche1.Arrancar(True)
+miCoche1.Estado()
+
+
+#Estado inicial del objeto
+#Encapsulacion
+print("--------------------------Segundo objeto--------------------------")
+miCoche2=Coche()
+miCoche2.Arrancar(False)
+miCoche2.Estado()
+miCoche2.Arrancar(True)
